ba91efd2d98b35204993bb0339302b72d2e5839b
Aayush Somani <44446368+Shinimani@users.noreply.github.com>
2018-10-25 00:57:34 +0530

    Added the 4 rotate functions

--- AVLTree.java
+++ AVLTree.java
@@ -44,6 +44,40 @@ public class AVLTree<T>
   {
     return x > y ? x : y;
   }
+  
+  private AVLNode rotateLL(AVLNode nodeTop)
+  {
+    AVLNode nodeLC = nodeTop.left;
+    nodeTop.left = nodeLC.right;
+    nodeLC.right = nodeTop;
+    nodeTop.height = max(height(nodeTop.left), height(nodeTop.right)) + 1;
+    nodeLC.height = max (height(nodeLC.right), nodeTop.height) + 1;
+    return nodeLC;
+  }
+
+  private AVLNode rotateRR(AVLNode nodeTop)
+  {
+    AVLNode nodeRC = nodeTop.right;
+    nodeTop.right = nodeRC.left;
+    nodeRC.left = nodeTop;
+    nodeTop.height = max(height(nodeTop.left), height(nodeTop.right)) + 1;
+    nodeRC.height = max(height(nodeTop) , height(nodeRC.right)) + 1;
+    return nodeRC;
+  }
+
+  private AVLNode rotateRL(AVLNode top)
+  {
+    top.left = rotateRR(top.left);
+    return rotateLL(top);
+  }
+
+  private AVLNode rotateLR(AVLNode top)
+  {
+    top.right = rotateLL(top.right);
+    return rotateRR(top);
+  }
+
+  
 
   
 
